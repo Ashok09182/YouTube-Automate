d75907eb915eaa9860f194a6ecaaea0c527cadf2
Ashok09182 <ashok09182@gmail.com>
2025-05-18 16:24:31 +0530

    Add files via upload

---
+++ ExcelDataProvider.java
@@ -0,0 +1,23 @@
+package demo.utils;
+
+import java.io.File;
+
+import org.testng.annotations.DataProvider;
+
+public class ExcelDataProvider {
+
+   @DataProvider(name = "excelData")
+    public static Object[][] excelData() {
+        String fileLocation = System.getProperty("user.dir") + File.separator + "src" +
+         File.separator + "test" + File.separator + "resources" + File.separator + "Search items.xlsx";
+
+        // Name of the worksheet within the Excel file
+        String worksheetName = "Sheet1";
+
+        // Column index containing the search terms
+        int columnIndex = 0;
+
+        System.out.println("Fetching excel file from "+fileLocation);
+        return ExcelReaderUtil.readExcelData(fileLocation, worksheetName, columnIndex);
+    }
+}
---
+++ TestCases.java
@@ -0,0 +1,194 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.logging.Level;
+
+import demo.utils.ExcelDataProvider;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+import demo.wrappers.Wrappers;
+//import dev.failsafe.internal.util.Assert;
+
+public class TestCases  { // Lets us read the data "extends ExcelDataProvider"
+        ChromeDriver driver;
+        SoftAssert softAssert = new SoftAssert();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        @Test(priority = 1, enabled = true)
+        public void testCase01() throws Exception {
+                System.out.println("Testcase_01 started");
+                // driver.get("https://www.youtube.com/");
+                Boolean URL = driver.getCurrentUrl().contains("youtube");
+                Assert.assertTrue(URL, "Youtube link is not correctly opened");
+
+                 // Locate the About link at the bottom of the sidebar and Click on "About"
+                By aboutLocator = By.xpath("//a[contains(text(), 'About')]");
+                Wrappers.scrollToViewport(driver, aboutLocator);
+                Wrappers.clickAW(driver, aboutLocator);
+
+                // Print the message displayed on the screen
+                By messageLocator = By.xpath("//section[contains(@class, 'about__content')]");
+                Wrappers.scrollToViewport(driver, messageLocator);
+                Wrappers.getDisplayedMessage(driver, messageLocator);
+                System.out.println("Testcase_01 ended");
+        }
+
+        @Test(priority = 2, enabled = true)
+        public void testCase02() throws Exception {
+                System.out.println("Testcase_02 started");
+                
+
+                 // Navigate to the "Films" tab
+                By filmsTab = By.xpath("//a[contains(@title, 'Films') or contains(@title, 'Movies')]");
+                Wrappers.scrollToViewport(driver, filmsTab);
+                Wrappers.clickAW(driver, filmsTab);
+
+                 // Scroll to the extreme right within the "Top Selling" section
+                String section = "Top selling";
+                Wrappers.scrollToExtreme(driver, By.xpath("//span[contains(text(), '"+ section +"')]/ancestor::div[contains(@class, 'item-section')]//button[contains(@aria-label, 'Next')]"));
+
+                // Last movie
+                By lastMovie = By.xpath("(//span[contains(text(), '"+ section +"')]/ancestor::div[contains(@class, 'item-section')]//ytd-grid-movie-renderer)[last()]");
+
+                
+                By filmCertificationLocator = By.xpath("//ytd-grid-movie-renderer[contains(@class,'style-scope')][16]//ytd-badge-supported-renderer/div[2]/p");
+                String filmCertification = Wrappers.getElementText(driver, lastMovie, filmCertificationLocator);
+
+                // String expected="U/A, U, A";
+               
+                softAssert.assertEquals(filmCertification, "U",  "The movie certification is not marked as 'A'");
+
+                By movieGenreLocator = By.xpath(".//span[contains(@class, 'metadata')]");
+                String movieGenre = Wrappers.getElementText(driver, lastMovie, movieGenreLocator);
+                
+                softAssert.assertTrue(movieGenre.contains("Comedy") || movieGenre.contains("Indian cinema"), "The movie genre is neither 'Comedy' nor 'Animation'");
+        
+                softAssert.assertAll();
+                System.out.println("Testcase_02 ended");
+
+        }
+
+        @Test(priority = 3, enabled = true)
+        public void testCase03() throws InterruptedException {
+
+                System.out.println("Testcase_03 started");
+
+                By musicTab = By.xpath("//a[contains(@title, 'Music')]");
+                Wrappers.scrollToViewport(driver, musicTab);
+                Wrappers.clickAW(driver, musicTab);
+
+                By showmore = By.xpath("(//span[contains(text(),'Biggest Hits')]/ancestor::div[@id='dismissible']//button)[1]");
+                Wrappers.scrollToExtreme(driver, showmore);
+                
+                String lastplaylist = driver.findElement(By.xpath("//span[text()='Bollywood Dance Hitlist']"))
+                                .getText();
+                                
+                System.out.println(lastplaylist);
+                WebElement songs = driver.findElement(By.xpath(
+                                "//span[text()='Bollywood Dance Hitlist']/../../../../../../a/yt-collection-thumbnail-view-model/yt-collections-stack/div/div[3]/yt-thumbnail-view-model/yt-thumbnail-overlay-badge-view-model/yt-thumbnail-badge-view-model/badge-shape/div[2]"));
+
+                String text = songs.getText().replaceAll("[^0-9]", "");
+                int number = Integer.parseInt(text);
+               
+                softAssert.assertTrue(number <= 50, "Number is not less or equal to 50");
+                softAssert.assertAll();
+                System.out.println("Testcase_03 ended");
+                
+         }
+
+        @Test(priority = 4, enabled = true)
+        public void testCase04() {
+                System.out.println("Testcase_04 started");
+                By newsTab = By.xpath("//a[contains(@title, 'News')]");
+                Wrappers.scrollToViewport(driver, newsTab);
+                Wrappers.clickAW(driver, newsTab);
+
+                // Locate the "Latest News Posts" section
+                By latestNewsPosts = By.xpath("//span[contains(text(), 'Latest news post')]");
+                Wrappers.scrollToViewport(driver, latestNewsPosts);
+
+                // Retrieve and print the body and the number of likes for each of the first 3
+                // news posts
+                int numberOfNewsPosts = 3;
+                By firstNNewsLocator = By.xpath(
+                                "(//span[contains(text(), 'Latest news post')]/ancestor::ytd-rich-section-renderer//ytd-post-renderer)[position() <= "+ numberOfNewsPosts + "]");
+                Wrappers.getBodyAndViewCount(driver, firstNNewsLocator);
+                System.out.println("Testcase_04 ended");
+        }
+        @Test(priority = 5, enabled = true, description = "Verify video views count", dataProvider = "excelData", dataProviderClass = ExcelDataProvider.class)
+        public void testCase05(String excelData) {
+                System.out.println("Testcase_05 started");
+                // Search for the item
+                By searchBox = By.xpath("//div[@id='center']//div/form/input");
+                Wrappers.sendKeysAW(driver, searchBox, excelData);
+
+                // Scroll through the search results until the total views for the videos reach
+                // 10 crore
+                long totalCount = 10_00_00_000;
+                Wrappers.scrollTillVideoCountReaches(driver, totalCount);
+                System.out.println("Testcase_05 ended");
+        }
+
+        /*
+         * TODO: Write your tests here with testng @Test annotation.
+         * Follow testCase01 testCase02... format or what is provided in
+         * instructions
+         */
+
+        /*
+         * Do not change the provided methods unless necessary, they will help in
+         * automation and assessment
+         */
+        
+        @BeforeMethod
+        public void driverGet() {
+                driver.get("https://www.youtube.com");
+        }
+
+        @BeforeTest
+                public void startBrowser() {
+                System.setProperty("java.util.logging.config.file", "logging.properties");
+
+                // NOT NEEDED FOR SELENIUM MANAGER
+                // WebDriverManager.chromedriver().timeout(30).setup();
+
+                ChromeOptions options = new ChromeOptions();
+                LoggingPreferences logs = new LoggingPreferences();
+
+                logs.enable(LogType.BROWSER, Level.ALL);
+                logs.enable(LogType.DRIVER, Level.ALL);
+                options.setCapability("goog:loggingPrefs", logs);
+                options.addArguments("--remote-allow-origins=*");
+
+                System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
+
+                driver = new ChromeDriver(options);
+
+                driver.manage().window().maximize();
+        }
+
+        @AfterTest
+        public void endTest() {
+                // driver.close();
+                driver.quit();
+
+        }
+}
